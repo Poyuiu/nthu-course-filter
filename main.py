74bd3b4bbf670f595462db833303b9ffb57d8d48
108000232 <jmyapple9@gmail.com>
2023-06-03 11:34:09 +0800

    [update] tabview: add seaching btn, add combobox

--- main.py
+++ main.py
@@ -43,6 +43,45 @@ class TimeSlot(customtkinter.CTkFrame):
     def init_buttons(self) -> None:
         pass
 
+class Tabview(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        # create tabview
+        self.tabview = customtkinter.CTkTabview(self, width=200)
+        self.tabview.grid(row=0, column=0, rowspan=2, padx=20, pady=10, sticky='n')
+        
+
+        # 3 search tabs (text)
+        self.tabview.add("major")
+        self.tabview.add("location")
+        self.tabview.add("Time")
+        # object position
+        self.tabview.tab("major").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("location").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Time").grid_columnconfigure(0, weight=1)
+        # searching UI: major
+        self.label_major = customtkinter.CTkLabel(self.tabview.tab("major"), text="Choose major below")
+        self.label_major.grid(row=0, column=0, padx=20, pady=20)
+        self.major_combobox = customtkinter.CTkComboBox(self.tabview.tab("major"),
+                                                    values=["請選擇...", "資工", "電機", "物理", "..."])
+        self.major_combobox.grid(row=1, column=0, padx=20, pady=(10, 10))
+        # searching UI: location
+        self.label_location = customtkinter.CTkLabel(self.tabview.tab("location"), text="Choose location below")
+        self.label_location.grid(row=0, column=0, padx=20, pady=20)
+        self.location_combobox = customtkinter.CTkComboBox(self.tabview.tab("location"),
+                                                    values=["請選擇...", "台達", "資電", "人社", "..."])
+        self.location_combobox.grid(row=1, column=0, padx=20, pady=(10, 10))
+
+        # searching UI: Time
+        self.label_time = customtkinter.CTkLabel(self.tabview.tab("Time"), text="Choose Time on RHS")
+        self.label_time.grid(row=0, column=0, padx=20, pady=20)
+        
+        # search button
+        self.search_button = customtkinter.CTkButton(self.tabview, text="Search", command=self.searching)
+        self.search_button.grid(row=4, column=0, padx=20, pady=10)
+
+    def searching(self) -> None:
+        print("searching, Nothing so far...")
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -50,38 +89,16 @@ class App(customtkinter.CTk):
         # configure window
         self.title("nthu-course-filter")
         self.geometry(f"{1100}x{670}")
-
-        # create tabview
-        self.tabview = customtkinter.CTkTabview(self, width=200)
-        self.tabview.grid(row=0, column=0, padx=20, pady=10, sticky='n')
-        self.tabview.add("major")
-        self.tabview.add("location")
-        self.tabview.add("Time")
-
-        self.tabview.tab("major").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("location").grid_columnconfigure(0, weight=1)
-
+        
+        # Time slot
         self.time_slot = TimeSlot(master=self)
         self.time_slot.grid(row=0, column=1, pady=20, sticky='n')
-        # self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("major"), dynamic_resizing=False,
-        #                                                 values=["Value 1", "Value 2", "Value Long Long Long"])
-        # self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        # self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("major"),
-        #                                             values=["Value 1", "Value 2", "Value Long....."])
-        # self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
-        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("major"), text="Open CTkInputDialog",
-        #                                                    command=self.open_input_dialog_event)
-        # self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
-        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("major"), text="Choose major below")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
-        
-        self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("location"), text="Choose location below")
-        self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
 
-        self.label_tab_3 = customtkinter.CTkLabel(self.tabview.tab("Time"), text="Choose Time on RHS")
-        self.label_tab_3.grid(row=0, column=0, padx=30, pady=20)
-        # self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("location"), text="CTkLabel on location")
-        # self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+        # Searching tabview
+        self.time_slot = Tabview(master=self)
+        self.time_slot.grid(row=0, column=0, pady=20, sticky='n')
+
+        
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
